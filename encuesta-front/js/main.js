1a3c1c095f4dd22d2835a3bab2d1f0503c333c62
Sebastian Ramos <seba.ragu98@gmail.com>
2024-08-27 18:29:43 -0400

    Front assets

---
+++ encuesta-front/js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+  'use strict';
+
+  /*[ Validate ]*/
+  var email = $('.validate-input input[name="email"]');
+
+  $('.validate-form').on('submit', function () {
+    var check = true;
+    if (
+      $(email)
+        .val()
+        .trim()
+        .match(
+          /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/,
+        ) == null
+    ) {
+      showValidate(email);
+      check = false;
+    }
+    return check;
+  });
+
+  $('.validate-form .input1').each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
+    });
+  });
+
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass('alert-validate');
+  }
+})(jQuery);
