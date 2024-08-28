7d966e23374701c21f38f0cc145c6d9b08e91b3d
Sebastian Ramos <seba.ragu98@gmail.com>
2024-08-27 23:37:01 -0400

    Final final final ahora si

--- src/modules/survey/survey.service.ts
+++ src/modules/survey/survey.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SurveyEntity } from './survey.entity';
+import { isEmail } from 'class-validator';
 
 @Injectable()
 export class SurveyService {
@@ -11,6 +12,10 @@ export class SurveyService {
   ) {}
 
   async create(survey: SurveyEntity): Promise<SurveyEntity> {
+    if (!isEmail(survey.email)) {
+      throw new HttpException('Invalid email format', HttpStatus.BAD_REQUEST);
+    }
+
     const emailExists = await this.surveyRepository.findOne({
       where: { email: survey.email },
     });
